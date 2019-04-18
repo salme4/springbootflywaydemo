632a655234e223cae9e189209c087bb4f0035ebd
salme1541 <dave.kim@nexr.com>
2019-04-18 09:49:48 +0900

    CON141 #1

--- README.md
+++ README.md
@@ -5,3 +5,5 @@ Spring boot with flyway sample project
 M1
 
 M2
+
+M3
---
+++ src/main/java/my/home/springbootjpademo/con141/Con141Class.java
@@ -0,0 +1,9 @@
+package my.home.springbootjpademo.con141;
+
+public class Con141Class {
+    public static void main(String[] args) {
+        System.out.println("hello");
+        System.out.println("hello2");
+        System.out.println("hello3");
+    }
+}
